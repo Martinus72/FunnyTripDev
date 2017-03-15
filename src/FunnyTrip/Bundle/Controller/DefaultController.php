1e6501be887f3d462b721b8406ece378abf35c6c
martin <martin@accolad.fr>
2017-03-15 13:11:21 +0100

    Test vue Trajet

--- src/FunnyTrip/Bundle/Controller/DefaultController.php
+++ src/FunnyTrip/Bundle/Controller/DefaultController.php
@@ -17,25 +17,37 @@ class DefaultController extends Controller
 
   /**
    * Affiche les trajets proposé par le user.
-   *
-   * @Route("/{id}", name="annonce_show")
-   * @Method("GET")
    */
-  public function trajetAction(Annonce $annonce)
+  public function trajetAction()
   {
-    $deleteForm = $this->createDeleteForm($annonce);
-    return $this->render('FunnyTripBundle:Default:reservation.html.twig', array(
-      'annonce' => $annonce,
-      'delete_form' => $deleteForm->createView(),
+    $em = $this->getDoctrine()->getManager();
+    /*$queryBuilder = $em->getRepository('FunnyTripBundle:Annonce')->createQueryBuilder('e');
+
+    list($annonces, $pagerHtml) = $this->paginator($queryBuilder, $request);
+
+    return $this->render('annonce/index.html.twig', array(
+      'annonces' => $annonces,
+      'pagerHtml' => $pagerHtml,
+
+    ));*/
+
+    $qb = $em->createQueryBuilder();
+    $qb->select('a')->from('FunnyTripBundle:Annonce', 'a')->where('a.id = :id')->setParameter('id', $this->getUser());
+    $annonces = $qb->getQuery()->getResult();
+
+    return $this->render('FunnyTripBundle:Default:trajet.html.twig', array(
+      'annonces' => $annonces,
     ));
-  }
 
-  public function reservationAction()
-  {
+}
 
-    return $this->render('FunnyTripBundle:Default:reservation.html.twig',
-      array('test' => 'Reservation'));
+public
+function reservationAction()
+{
 
-  }
+  return $this->render('FunnyTripBundle:Default:reservation.html.twig',
+    array('test' => 'Reservation'));
+
+}
 
 }
